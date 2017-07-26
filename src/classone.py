c993b5343b370424c10844d16d2b7d0e87264b50
Pieter Holthuijsen <>
2017-07-26 14:25:15 +0200

    Class and Exceptions

--- src/classone.py
+++ src/classone.py
@@ -4,25 +4,25 @@
 # Author:       Pieter Holthuijsen
 # ================================================
 
+
 class Employee:
     # Common base class.
     empcount = 0
 
-    def __init__(self, pName, pSalary):
-        self.lName = pName
-        self.lSalary = pSalary
+    def __init__(self, p_name, p_salary):
+        self.l_name = p_name
+        self.l_salary = p_salary
         Employee.empcount += 1
 
     def __del__(self):
-        print(self.lName + " destroyed!")
+        print(self.l_name + " destroyed!")
         Employee.empcount -= 1
 
-    def displayCount(self):
+    def display_count(self):
         print("total Employees is %d" % Employee.empcount)
 
-    def displayEmployee(self):
-        print("Name: ", self.lName, ", Salary: ", self.lSalary)
-
+    def display_employee(self):
+        print("Name: ", self.l_name, ", Salary: ", self.l_salary)
 
 
 e1 = Employee("Piet", 3000)
@@ -30,20 +30,17 @@ e2 = Employee("Klaas", 2000)
 e3 = Employee("Bert", 1000)
 
 try:
-    e1.displayCount()
-    e2.displayCount()
-    e3.displayCount()
-    e1.displayEmployee()
-
+    e1.display_count()
+    e2.display_count()
+    e3.display_count()
+    e1.display_employee()
     del e1
-    e3.displayCount()
+    e3.display_count()
     try:
-        e1.displayCount() # Error
+        e1.display_count()  # Error
     except NameError:
         print("Error: Object does not exist.")
-    e2.displayCount()
+    e2.display_count()
 
 except NameError:
     print("Error: Object does not exist.")
-
-
