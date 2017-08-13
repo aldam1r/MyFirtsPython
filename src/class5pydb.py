7dbf93d46c81fabb7c4822afb3ec7c5fa07a0d43
Pieter Holthuijsen <>
2017-08-13 19:03:45 +0200

    Class and Exceptions

--- src/class5pydb.py
+++ src/class5pydb.py
@@ -5,16 +5,37 @@
 # ================================================
 
 
-import pymysql
+from pymysql import *
 
-db = pymysql.connect("192.168.178.101","testuser","Test-123","pydb")
+def connectiontest():
+    db = pymysql.connect("192.168.178.101","testuser","Test-123","pydb")
+    cursor = db.cursor()
+    cursor.execute("select version();")
+    data = cursor.fetchone()
+    print ("Database version : %s " % data)
+    db.close ()
 
-cursor = db.cursor()
+def droptable():
+    db = pymysql.connect("192.168.178.101","testuser","Test-123","pydb")
+    cursor = db.cursor()
+    stmt = """drop table if exists employee"""
+    cursor.execute(stmt)
+    db.close()
 
-cursor.execute("select version();")
+def cretable():
+    db = pymysql.connect("192.168.178.101","testuser","Test-123","pydb")
+    cursor = db.cursor()
+    stmt = """create table employee
+              ( first_name      char(30)    not null
+              , last_name       char(30)
+              , age             integer
+              , sex             char(2)
+              , income          float
+              )"""
+    cursor.execute(stmt)
+    db.close()
 
-data = cursor.fetchone()
 
-print ("Database version : %s " % data)
-
-db.close
+# connectiontest()
+# droptable()
+# cretable()
